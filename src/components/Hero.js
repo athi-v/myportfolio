362b883216ec77343b687949d5328f2d20e3d190
Athenkosi Vinqi <nervadevelop@gmail.com>
2022-01-30 19:42:48 +0200

    added n

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-scroll'
-import NewWindow from 'react-new-window'
+import fike from './assets/Quotation_2.pdf';
 import './Herostyles.css';
 const Hero = () => {
   return (
@@ -12,7 +12,7 @@ const Hero = () => {
             <h5>Welcome to my personal portfolio..</h5>
             <h6>Freelance Frontend & Backend Developer</h6>
 
-            <a href='./Quotation_2.pdf' target='_blank'>
+            <a href={fike} target='_blank'>
 <button className='btn btn-primary'>Download CV</button>
 </a>            <button className='btn btn-primary'><Link to='contact'>Contact Me</Link></button>
 
